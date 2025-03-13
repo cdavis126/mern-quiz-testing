ba42e67123d5caf99f812225bce477bfe4d6a507
Cherie Davis <cheriedavis126@gmail.com>
2025-03-13 11:28:09 -0400

    updating ete.ts file

---
+++ cypress/support/ete.ts
@@ -0,0 +1,6 @@
+// ***********************************************************
+// This file runs automatically before your E2E tests.
+// ***********************************************************
+
+// Import your custom commands
+import './commands';
