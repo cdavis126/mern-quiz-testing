6569e793ce529c0b1897bbedc26e2759b61acf23
Cherie Davis <cheriedavis126@gmail.com>
2025-03-13 11:31:23 -0400

    final cypress files

--- cypress/support/component-setup.tsx
+++
@@ -1,7 +0,0 @@
-////come back to this later
-
-import './commands';
-import { mount } from 'cypress/react18';
-
-// Correctly registers 'cy.mount()' command globally
-Cypress.Commands.add('mount', mount);
